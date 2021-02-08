742e778cfcb79a1e7fc2b390f4f3b4c9a15105f2
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-02-07 18:01:11 -0700

    add more data files/points

--- CS301-Assignment2.py
+++ CS301-Assignment2.py
@@ -13,14 +13,20 @@ def bench(f, inp):
 
 listData1k = list(open('data1k.txt').read().split())
 listData10k = list(open('data10k.txt').read().split())
+listData50k = list(open('data50k.txt').read().split())
 listData100k = list(open('data100k.txt').read().split())
+listData250k = list(open('data.250k.txt').read().split())
 listData500k = list(open('data500k.txt').read().split())
+listData750k = list(open('data750k.txt').read().split())
 listData1m = list(open('data1m.txt').read().split())
 
 dictData1k = {}
 dictData10k = {}
+dictData50k = {}
 dictData100k = {}
+dictData250k = {}
 dictData500k = {}
+dictData750k = {}
 dictData1m = {}
 for line in listData1k:
     if line not in dictData1k:
@@ -28,19 +34,28 @@ for line in listData1k:
 for line in listData10k:
     if line not in dictData10k:
         dictData10k[line] = 1
+for line in listData50k:
+    if line not in dictData50k:
+        dictData50k[line] = 1
 for line in listData100k:
     if line not in dictData100k:
         dictData100k[line] = 1
+for line in listData250k:
+    if line not in dictData250k:
+        dictData250k[line] = 1
 for line in listData500k:
     if line not in dictData500k:
         dictData100k[line] = 1
+for line in listData750k:
+    if line not in dictData750k:
+        dictData750k[line] = 1
 for line in listData1m:
     if line not in dictData1m:
         dictData1m[line] = 1
 
-listOfLists = [listData1k, listData10k, listData100k, listData500k, listData1m]
-listOfDicts = [dictData1k, dictData10k, dictData100k, dictData500k, dictData1m]
-dataSize= [1000, 10000, 100000, 500000, 1000000]
+listOfLists = [listData1k, listData10k, listData50k, listData100k, listData250k, listData500k, listData750k, listData1m]
+listOfDicts = [dictData1k, dictData10k, dictData50k, dictData100k, dictData250k, dictData500k, dictData750k, dictData1m]
+dataSize= [1000, 10000, 50000, 100000, 250000, 500000, 750000, 1000000]
 
 def main():
     f_out = open("AppendingToListsData.csv", "w+")
